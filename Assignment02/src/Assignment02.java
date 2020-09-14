f7f513cbdc9d41af7d2d6ccb4e8d7b785d500a38
Donovan Moses <49540973+donovanjmoses@users.noreply.github.com>
2020-09-14 18:14:47 -0500

    Update Assignment02.java

--- Assignment02/src/Assignment02.java
+++ Assignment02/src/Assignment02.java
@@ -1,8 +1,3 @@
-// CSE 110     : 75326 | ONLINE
-// Assignment  : ASSIGNMENT 2
-// Author      : DONOVAN MOSES / 1210366219
-// Description : A PROGRAM THAT CALCULATES MATERIALS AND COSTS OF A CONSTRUCTION PROJECT
-
 import java.util.Scanner;
 
 public class Assignment02 {
@@ -17,11 +12,7 @@ public class Assignment02 {
         int numberOfLanes;
         int depthOfAsphault;
         int numberOfDays;
-        
-        
-       
-       
-        
+      
         // collect inputs
         System.out.println("What is the length of the road project in miles? ");
         roadLength = in.nextDouble();
@@ -31,9 +22,7 @@ public class Assignment02 {
         depthOfAsphault = in.nextInt();
         System.out.println("How many days to complete the project? ");
         numberOfDays = in.nextInt();
-        
-       
-        
+               
         // declare and initialize variables to compute required values for Truckloads of Asphault output
         int laneWidth;
         int laneLength;
@@ -41,7 +30,6 @@ public class Assignment02 {
         int asphaultVolume;
         int asphaultWeight;
         int truckloadsOfAsphault;
-        
         
         // compute required values for Truckloads of Asphault output
         laneWidth = 12; // measured in feet
@@ -51,84 +39,57 @@ public class Assignment02 {
         asphaultWeight = (asphaultVolume * 150);
         truckloadsOfAsphault = (int) (asphaultWeight / 10000);
         
-        
-        
         // declare and initialize variables to compute required values for Stoplights
         int stoplightsPerIntersection;
         int numberOfStoplights;
         
-        
-        
         // compute required values for Stoplights
         stoplightsPerIntersection = (int) Math.floor(roadLength * 2);
         numberOfStoplights = (stoplightsPerIntersection + numberOfLanes);
-        
-        
-        
+               
         //declare and initialize variables to compute required values for Conduit Pipes
         double numberOfConduits = Math.ceil(laneLength / 24);
-        
-        
-        
+       
         // declare and initialize variables to compute required values for Crew Members
         int numberOfCrewMembers;
         
-        
-        
         // compute required values for Crew Members
         numberOfCrewMembers = (int) Math.ceil(50 * roadLength * numberOfLanes) / numberOfDays;
-        
-        
-        
+       
         // declare and initialize variables to compute required values for Cost of Asphault
         int costPerTruckload;
         int totalCostOfAsphault;
-        
-        
+    
         // compute required values for Cost of Asphault
         costPerTruckload = 1000;
         totalCostOfAsphault = (costPerTruckload * truckloadsOfAsphault);
-        
-        
-        
+     
         // declare and initialize variables to compute required values for Cost of Stoplights
         int totalCostOfStoplights;
-        
-        
+     
         // compute required values for Cost of Stoplights
         totalCostOfStoplights = (numberOfStoplights * 25000);
-        
-        
+            
         // declare and initialize variables to compute required values for Cost of Conduit Pipes
         int totalCostOfConduits;
-        
-        
+     
         // compute required values for Cost of Conduit Pipes
         totalCostOfConduits = (int) (numberOfConduits * 500);
-        
-        
+                
         // declare and initialize variables to compute required values for Cost of Labor
         int crewMemberDailyPayRate;
         int totalCostOfLabor;
-        
-        
-        
+             
         // compute required values for Cost of Labor
         crewMemberDailyPayRate = (8 * 25);
         totalCostOfLabor = (crewMemberDailyPayRate * numberOfDays);
-        
-        
-        
+            
         // declare and initialize variables to compute required values for Cost of Labor
         int totalCostOfProject;
-        
-        
-        
+       
         // compute required values for Cost of Labor
         totalCostOfProject = (totalCostOfAsphault + totalCostOfStoplights + totalCostOfConduits + totalCostOfLabor);
-        
-        
-        
+  
         // display results
         System.out.println("=== Amount of materials needed ===");
         System.out.println("Truckloads of Asphault: " + Math.ceil(truckloadsOfAsphault * numberOfLanes));
@@ -141,7 +102,6 @@ public class Assignment02 {
         System.out.println("Cost of conduits: " + totalCostOfConduits);
         System.out.println("Cost of labor: " + totalCostOfLabor);
         System.out.println("=== Total Cost of Project ========");
-        System.out.println("Total cost of project: " + totalCostOfProject);
-                
+        System.out.println("Total cost of project: " + totalCostOfProject);         
     }
 }
